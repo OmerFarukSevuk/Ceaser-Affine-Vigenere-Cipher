961421879664735660410cad7453b2ee0fb44352
Ömer Faruk Sevük <o.farukrt@gmail.com>
2022-06-12 12:04:37 +0300

    adding affine cipher

--- decoder.py
+++ decoder.py
@@ -8,7 +8,7 @@ def Ceaser():
     newalph2=""
     for i in range(len(alph)):
         newalph += alph[(i-shift)%len(alph)]
-    downalph=alph.lower()
+    lowalph=alph.lower()
     for i in range(len(text)):
         char=text[i]
         for a in range(len(alph)):
@@ -16,7 +16,7 @@ def Ceaser():
             if char==char2:
                 decrypted+=alph[a]
             elif char.lower()==char2.lower():
-                decrypted+=downalph[a]
+                decrypted+=lowalph[a]
     for i in range(len(alph)):
         newalph2 += alph[(i+shift)%len(alph)]
     for i in range(len(text)):
@@ -26,6 +26,26 @@ def Ceaser():
             if char==char2:
                 decrypted2+=alph[a]
             elif char.lower()==char2.lower():
-                decrypted2+=downalph[a]
+                decrypted2+=lowalph[a]
     print(f"Decrypted({-shift}): {decrypted2}")
     print(f"Decrypted({shift}): {decrypted}")
+def Affine():
+    cipher=""
+    key_a=int(input("Key A: "))
+    key_b=int(input("Key B: "))
+    alph=input("Alphabet: ").upper()
+    text=input("Text: ")
+    lowalph=alph.lower()
+    z=0
+    while not((key_a*z)%len(alph)==1):
+        z+=1
+    for i in range(len(text)):
+        char=text[i]
+        for a in range(len(alph)):
+            char2=alph[a]
+            if char==char2:
+                cipher+=alph[(z*(a-key_b))%len(alph)]
+            elif char.lower()==char2.lower():
+                cipher+=lowalph[(z*(a-key_b))%len(alph)]
+    print(f"Decrypted: {cipher}")
+Affine()
--- encoder.py
+++ encoder.py
@@ -6,7 +6,7 @@ def Ceaser():
     newalph=""
     for i in range(len(alph)):
         newalph += alph[(i+shift)%len(alph)]
-    newdownalph=newalph.lower()    
+    newlowalph=newalph.lower()    
     for i in range(len(text)):
         char=text[i]
         for a in range(len(alph)):
@@ -14,5 +14,22 @@ def Ceaser():
             if char==char2:
                 cipher+=newalph[a]
             elif char.lower()==char2.lower():
-                cipher+=newdownalph[a]
+                cipher+=newlowalph[a]
     print(f"Encrypted: {cipher}")
+def Affine():
+    cipher=""
+    key_a=int(input("Key A: "))
+    key_b=int(input("Key B: "))
+    alph=input("Alphabet: ").upper()
+    text=input("Text: ")
+    lowalph=alph.lower()
+    for i in range(len(text)):
+        char=text[i]
+        for a in range(len(alph)):
+            char2=alph[a]
+            if char==char2:
+                cipher+=alph[((a*key_a)+key_b)%len(alph)]
+            elif char.lower()==char2.lower():
+                cipher+=lowalph[((a*key_a)+key_b)%len(alph)]
+    print(f"Encrypted: {cipher}")
+Affine()
